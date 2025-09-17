d6906e5497a8f907a82d55295d21c08ee203a593
Sgintoki-jr <sgintokijr@gmail.com>
2025-09-16 22:03:01 -0600

    Lista de Amigos creada en HTML app.js

--- app.js
+++ app.js
@@ -14,4 +14,19 @@ function agregarAmigo() {
     // Borrar el nombre
     input.value = ''; 
 
+    // Actualizar la lista visible
+    mostrarLista();
 }
+
+// Paso 3: Recorrer el array y mostrarlo como <li> dentro de la lista
+function mostrarLista() {
+    const listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.innerHTML = ""; // Limpiar datos
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaDeAmigos.appendChild(li);
+    }
+}
+
